257f6f5981cd77a65d268e82a796d65fcf2e009b
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-02 12:51:41 +0100

    add day01project/2

--- src/main/java/adventofcode/day01/Measurement.java
+++ src/main/java/adventofcode/day01/Measurement.java
@@ -9,22 +9,42 @@ import java.util.List;
 
 public class Measurement {
 
-    private List<String>measurements = new ArrayList<>();
+    private List<String> measurements = new ArrayList<>();
+    private List<Integer> measurementSumOfThree = new ArrayList<>();
 
     public void addToList(Path path) throws IOException {
         List<String> temp = Files.readAllLines(path);
         measurements = temp;
     }
 
-    public int getNumberOfIncreasing(List<String> measurements){
+    public int getNumberOfIncreasing(List<String> measurements) {
         int counter = 0;
-        for (int i = 1; i < measurements.size(); i++){
-            if(Integer.parseInt(measurements.get(i)) > Integer.parseInt(measurements.get(i - 1))){
+        for (int i = 1; i < measurements.size(); i++) {
+            if (Integer.parseInt(measurements.get(i)) > Integer.parseInt(measurements.get(i - 1))) {
                 counter++;
             }
         }
         return counter;
     }
+
+    public void getSumOfThree(List<String> measures) {
+        for (int i = 0; i < measures.size() - 2; i++) {
+            measurementSumOfThree.add(Integer.parseInt(measures.get(i)) +
+                    Integer.parseInt(measures.get(i + 1)) +
+                    Integer.parseInt(measures.get(i + 2)));
+        }
+    }
+
+    public int getNumberOfSumsIncreasing(List<Integer> sums){
+        int counter = 0;
+        for(int i = 1;i < sums.size(); i++){
+            if(sums.get(i) > sums.get(i -1)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 
     public List<String> getMeasurements() {
         return measurements;
@@ -32,12 +52,16 @@ public class Measurement {
 
     public static void main(String[] args) {
         Measurement measurement = new Measurement();
-        try{
+        try {
             measurement.addToList(Paths.get("src/main/resources/measurement.txt"));
-        }catch (IOException ioe){
-            throw new IllegalStateException("Cant read",ioe);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Cant read", ioe);
         }
         System.out.println(measurement.getNumberOfIncreasing(measurement.measurements));
 
+        measurement.getSumOfThree(measurement.measurements);
+        System.out.println(measurement.getNumberOfSumsIncreasing(measurement.measurementSumOfThree));
+
     }
+
 }
